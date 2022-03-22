c8a72b8991b486598b730739065d70bd806aa8b2
mica <micaeladominguez@sirius.com.ar>
2022-03-22 11:55:17 -0300

    Ej2c

--- src/tp1/Ejs.java
+++ src/tp1/Ejs.java
@@ -14,9 +14,29 @@ public class Ejs {
     private int exercise_1_c_recursive_aux(int p, int n, int i ){
         if(i <= n){
             return (int)(Math.pow(p, i) + exercise_1_c_recursive_aux(p, n, ++ i ));
-        }else{
-            return 0;
         }
-
+        return 0;
+    }
+    public int exercise_2_c_iterative(int n){
+        if(n > 1){
+            int fib0 = 1;
+            int fib1= 1;
+            for (int i = 3; i <= n; i++) {
+                int aux = fib0 + fib1;
+                fib0= fib1;
+                fib1= aux;
+            }
+            return fib1;
+        }else{
+            return 1;
+        }
+    }
+    public int exercise_2_c_recursive(int n){
+        return exercise_2_c_recursive_aux(n);
+    }
+    private int exercise_2_c_recursive_aux(int n){
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+        else return exercise_2_c_recursive_aux(n - 1) + exercise_2_c_recursive_aux(n - 2);
     }
 }
--- src/tp1/Main.java
+++ src/tp1/Main.java
@@ -3,9 +3,10 @@ package tp1;
 public class Main {
     public static void main(String[] args) {
         Ejs ejs = new Ejs();
-        System.out.println(ejs.exercise_1_c_iterative(2,3));
-        System.out.println(ejs.exercise_1_c_recursive(2,3));
-
+        /*System.out.println(ejs.exercise_1_c_iterative(2,3));
+        System.out.println(ejs.exercise_1_c_recursive(2,3));*/
+        System.out.println(ejs.exercise_2_c_iterative(6));
+        System.out.println(ejs.exercise_2_c_recursive(6));
 
     }
 }
