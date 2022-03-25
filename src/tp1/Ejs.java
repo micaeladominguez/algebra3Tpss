24ecbe472575404f99e5a294e294408ea9f44eb4
mica <micaeladominguez@sirius.com.ar>
2022-03-25 09:17:26 -0300

    Tp1

--- src/tp1/Ejs.java
+++ src/tp1/Ejs.java
@@ -39,4 +39,68 @@ public class Ejs {
         else if (n == 1) return 1;
         else return exercise_2_c_recursive_aux(n - 1) + exercise_2_c_recursive_aux(n - 2);
     }
+    public boolean exercise_4_iterative(int[] array){
+        int j= array.length -1;
+        int i = 0;
+        while(j > i){
+            if(array[i] != array[j]) return false;
+            i += 1;
+            j -= 1;
+        }
+        return true;
+    }
+    public boolean exercise_4_recursive(int[] array){
+        return exercise_4_recursive_aux(array,0, array.length -1);
+    }
+
+    private boolean exercise_4_recursive_aux(int[] array, int i, int j) {
+        if(j <= i) return true;
+        if(array[i] != array[j]) return false;
+        return exercise_4_recursive_aux(array, i + 1, j - 1);
+    }
+
+    public int exercise_6_b_iii_iterative(int n){
+        if(isAPrimeNumber(n) && n != 1) return 2;
+        int counter = 0;
+        for (int i = 1; i <=  n/2; i++) {
+            if (n % i == 0 && isAPrimeNumber(i)) counter +=1;
+        }
+        return counter;
+    }
+    public boolean isAPrimeNumber(int n){
+        if(n == 1) return true;
+        int counter = 0;
+        for (int i = 1; i <=  n/2 ; i++) {
+            if(n % i == 0) counter += 1;
+        }
+        return counter <= 1;
+    }
+    public int exercise_6_b_iii_recursive(int n){
+        if(isAPrimeNumber(n)) return 1 + exercise_6_b_iii_recursive_aux(n,1);
+        return exercise_6_b_iii_recursive_aux(n, 1);
+    }
+    private int exercise_6_b_iii_recursive_aux(int n, int i) {
+        if( i > n/2) return 0;
+        if(n % i == 0 && isAPrimeNumber(i)) return 1 + exercise_6_b_iii_recursive_aux(n, i + 1);
+        return exercise_6_b_iii_recursive_aux(n, i + 1);
+    }
+    public int exercise_8_iterative(int[] coefs, int n){
+        int count = 0;
+        for (int i = coefs.length - 1; i > 0 ; i--) {
+            count += coefs[i];
+            count = count * n;
+        }
+        return count + coefs[0];
+    }
+    public int exercise_8_recursive(int[] coefs, int n){
+        return exercise_8_recursive_aux(coefs, n , coefs.length - 1);
+    }
+
+    private int exercise_8_recursive_aux(int[] coefs, int n, int pointer) {
+        if(pointer == 0){
+            return coefs[0];
+        }else{
+            return coefs[pointer] * n + exercise_8_recursive_aux(coefs, n, pointer - 1);
+        }
+    }
 }
--- src/tp1/Main.java
+++ src/tp1/Main.java
@@ -5,8 +5,16 @@ public class Main {
         Ejs ejs = new Ejs();
         /*System.out.println(ejs.exercise_1_c_iterative(2,3));
         System.out.println(ejs.exercise_1_c_recursive(2,3));*/
-        System.out.println(ejs.exercise_2_c_iterative(6));
-        System.out.println(ejs.exercise_2_c_recursive(6));
-
+        /*System.out.println(ejs.exercise_2_c_iterative(6));
+        System.out.println(ejs.exercise_2_c_recursive(6));*/
+        int[] array = new int[4];
+        array[0] = 1;
+        array[1] = 8;
+        array[2] = 5;
+        array[3] = 10;
+       // System.out.println(ejs.exercise_4_recursive(array));
+        // System.out.println(ejs.exercise_6_b_iii_recursive(5));
+        //System.out.println(ejs.exercise_8_iterative(array, 2));
+        System.out.println(ejs.exercise_8_recursive(array, 2));
     }
 }
